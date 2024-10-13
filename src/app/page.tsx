1cddda3a1e42f12facc41d13a92cabf857a325fd
Bruno Aseff <155178870+BrunoAseff@users.noreply.github.com>
2024-10-13 14:56:45 +0000

    adds testing image

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,7 @@
+import { ArrowRight } from "@/components/icons/arrow-right";
 import Title from "@/components/title";
 import { Button } from "@/components/ui/button";
+import Image from "next/image";
 import Link from "next/link";
 
 export default function HomePage() {
@@ -7,6 +9,16 @@ export default function HomePage() {
     <main className="flex min-h-screen flex-col items-center justify-center bg-black text-white">
       <div className="container flex flex-col items-center justify-center gap-12 px-4 py-16">
         <Title>Nova</Title>
+      </div>
+      <div className="relative mb-12">
+        <div className="absolute -inset-2 rounded-lg bg-[conic-gradient(at_bottom_left,_var(--tw-gradient-stops))] from-primary via-secondary to-secondary opacity-50 blur-2xl"></div>
+        <Image
+          className="relative rounded-lg"
+          src="/testImage.jpg"
+          alt="Nova"
+          width={450}
+          height={450}
+        />
       </div>
 
       <Button
@@ -16,9 +28,16 @@ export default function HomePage() {
       >
         Get started
       </Button>
-      <Link className="mt-4 text-primary-foreground" href="#">
-        Continue without login
-      </Link>
+      <Button variant="link">
+        {" "}
+        <Link
+          className="mt-8 flex items-center text-primary-foreground"
+          href="/spaces"
+        >
+          Continue without login
+          <ArrowRight color="#D1E9FF" />
+        </Link>
+      </Button>
     </main>
   );
 }
