90cbda02e24de639b84262123cf7d388179a5f7e
Bruno Aseff <155178870+BrunoAseff@users.noreply.github.com>
2024-12-02 20:59:47 +0000

    feat: add breathing exercise files

--- src/components/features/breathingExercise/BreathingExercise.tsx
+++ src/components/features/breathingExercise/BreathingExercise.tsx
@@ -1,12 +1,27 @@
+import { Button } from "@/components/nova/buttons/Button";
 import type { breathingExerciseProps } from "@/types";
+import BoxBreathing from "./BoxBreathing";
+import { useState } from "react";
 
 export default function BreathingExercise({
   isHidden,
+  technique,
 }: breathingExerciseProps) {
   // technique type is "4-7-8" | "Box Breathing" | "Alternate Nostril Breathing" | "Wim Hof Method"
+
+  const [isExerciseRunning, setIsExerciseRunning] = useState(false);
+
   if (isHidden) return null;
 
   return (
-    <div className="fixed left-1/2 top-1/2 flex h-[28rem] w-[28rem] -translate-x-1/2 -translate-y-1/2 transform flex-col items-center justify-center rounded-full bg-secondary-smooth-700/30 text-foreground"></div>
+    <div className="fixed left-1/2 top-1/2 flex h-[28rem] w-[28rem] -translate-x-1/2 -translate-y-1/2 transform flex-col items-center justify-center rounded-full bg-secondary-smooth-700/30 text-foreground">
+      {isExerciseRunning ? (
+        <div>{technique === "Box Breathing" && <BoxBreathing />}</div>
+      ) : (
+        <Button onClick={() => setIsExerciseRunning(!isExerciseRunning)}>
+          Start
+        </Button>
+      )}
+    </div>
   );
 }
