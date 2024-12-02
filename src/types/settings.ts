a6bdb0a6c955445606949afc4da384fe95ea728c
Bruno Aseff <155178870+BrunoAseff@users.noreply.github.com>
2024-12-02 15:49:41 +0000

    refactor: make `settings` more semantic

---
+++ src/types/settings.ts
@@ -0,0 +1,39 @@
+import type { ShortcutName, Space } from "@/types";
+
+export interface SpaceContextValue {
+  spaces: Space[];
+  selectedTab: string;
+  selectTab: (tab: string) => void;
+  updateSpaceProperty: (
+    spaceName: string,
+    propertyName: keyof Space,
+    value: any,
+  ) => void;
+  updateSpaceSharedProperty: (propertyName: "isHidden", value: boolean) => void;
+  playPomodoroAlarm: () => Promise<void>;
+  stopPomodoroAlarm: ({ currentSpace }: { currentSpace: Space }) => void;
+  isAlarmPlaying: boolean;
+  shortcut: ShortcutName;
+  ambientSound: string;
+  updateShortcut: (newShortcut: ShortcutName) => void;
+  ambientSoundVolume: number;
+  isAmbientSoundPlaying: boolean;
+  playAmbientSound: (soundUrl?: string) => void;
+  pauseAmbientSound: () => void;
+  updateAmbientSound: (soundUrl: string) => void;
+  updateAmbientSoundVolume: (volume: number) => void;
+  toggleAmbientSound: () => void;
+  resetSpaces: () => void;
+  setSpaces: (spaces: Space[]) => void;
+  setShortcut: (shortcut: ShortcutName) => void;
+  setAmbientSound: (ambientSound: string) => void;
+  setAmbientSoundVolume: (volume: number) => void;
+}
+
+export interface settingsType {
+  spaces: Space[];
+  shortcut: ShortcutName;
+  ambientSound: string;
+  ambientSoundVolume: number;
+  isAmbientSoundPlaying: boolean;
+}
