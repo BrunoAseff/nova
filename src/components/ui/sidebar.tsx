b7ee6ab74e482b0b48872027be92af1d0076fb38
Bruno Aseff <155178870+BrunoAseff@users.noreply.github.com>
2024-12-04 16:35:41 +0000

    refactor: remove animation

--- src/components/ui/sidebar.tsx
+++ src/components/ui/sidebar.tsx
@@ -233,7 +233,7 @@ const Sidebar = React.forwardRef<
         >
           <div
             className={cn(
-              "fixed inset-y-0 z-10 hidden h-svh w-[--sidebar-width] transition-[left,right,width] duration-200 ease-linear md:flex",
+              "fixed inset-y-0 z-50 hidden h-svh w-[--sidebar-width] transition-[left,right,width] duration-200 ease-linear md:flex",
               "right-0 group-data-[collapsible=offcanvas]:right-[calc(var(--sidebar-width)*-1)]",
               // Adjust the padding for floating and inset variants.
               variant === "floating" || variant === "inset"
