52fa3bb2dec92547d9677fc317cc12fa2b030c5a
Bruno Aseff <155178870+BrunoAseff@users.noreply.github.com>
2024-12-18 13:29:17 +0000

    fix: change title

--- src/components/nova/title.tsx
+++ src/components/nova/title.tsx
@@ -1,6 +1,8 @@
+import React from "react";
+
 export default function Title({ children }: { children: React.ReactNode }) {
   return (
-    <h1 className="bg-gradient-to-br from-primary from-10% via-secondary via-50% to-primary-foreground to-80% bg-clip-text text-5xl font-medium tracking-tight text-transparent sm:text-[5rem]">
+    <h1 className="bg-gradient-to-br from-[#1AECFF] via-[#57ABFF] to-[#D1E9FF] bg-clip-text text-5xl font-medium tracking-tight text-transparent sm:text-[5rem]">
       {children}
     </h1>
   );
