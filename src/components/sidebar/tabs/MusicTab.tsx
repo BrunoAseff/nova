72a6f5529334f60cd758164aed98fd28903501f5
Bruno Aseff <brunoaseff2@gmail.com>
2025-01-02 16:29:47 -0300

    feat: add card to music tab

--- src/components/sidebar/tabs/MusicTab.tsx
+++ src/components/sidebar/tabs/MusicTab.tsx
@@ -1,5 +1,8 @@
+import PrimaryBtn from "@/components/nova/buttons/PrimaryBtn";
 import MusicIllustration from "@/components/svgs/MusicIllustration";
 import { TabHeader } from "@/components/tabHeader";
+import { Label } from "@/components/ui/label";
+import Link from "next/link";
 
 export default function MusicTab() {
   return (
@@ -9,7 +12,20 @@ export default function MusicTab() {
         subtitle="Manage your music library to enhance your listening experience."
         Icon={MusicIllustration}
       />
-      <div className="mt-28 flex h-full flex-col gap-10"></div>
+      <div className="mx-auto mt-28 flex h-full w-[110%] flex-col gap-6">
+        <div className="via mt-6 flex min-h-16 w-full flex-col items-center gap-6 space-x-2 rounded-2xl border-[1px] border-secondary/60 bg-background p-4 shadow-[0px_20px_207px_10px] shadow-secondary/40">
+          <Label className="text-md px-3 font-montserrat text-foreground">
+            Upgrade to{" "}
+            <span className="font-semibold text-secondary">Supernova</span> to
+            access custom playlists.
+          </Label>
+          <PrimaryBtn className="rounded-xl border-[1px] border-secondary/60 bg-secondary-smooth-700/10 px-4 py-3 text-sm text-foreground transition-all duration-1000 hover:rounded-full hover:bg-secondary-smooth-700/10">
+            <Link href="/pricing">
+              <p>Upgrade</p>
+            </Link>
+          </PrimaryBtn>
+        </div>
+      </div>{" "}
     </main>
   );
 }
