c2d71445245d66cd3d01c4b65295814a93770842
Bruno Aseff <brunoaseff2@gmail.com>
2025-01-04 15:13:30 -0300

    style: make music tab responsive

--- src/components/sidebar/tabs/MusicTab.tsx
+++ src/components/sidebar/tabs/MusicTab.tsx
@@ -12,14 +12,14 @@ export default function MusicTab() {
         subtitle="Manage your music library to enhance your listening experience."
         Icon={MusicIllustration}
       />
-      <div className="mx-auto mt-28 flex h-full w-[110%] flex-col gap-6">
+      <div className="mx-auto mt-28 flex h-full w-[135%] flex-col gap-6 md:w-[110%]">
         <div className="via mt-6 flex min-h-16 w-full flex-col items-center gap-6 space-x-2 rounded-2xl border-[1px] border-secondary/60 bg-background p-4 shadow-[0px_20px_207px_10px] shadow-secondary/40">
           <Label className="text-md px-3 font-montserrat text-foreground">
             Upgrade to{" "}
             <span className="font-semibold text-secondary">Supernova</span> to
             access custom playlists.
           </Label>
-          <PrimaryBtn className="rounded-xl border-[1px] border-secondary/60 bg-secondary-smooth-700/10 px-4 py-3 text-sm text-foreground transition-all duration-1000 hover:rounded-full hover:bg-secondary-smooth-700/10">
+          <PrimaryBtn className="rounded-xl border-[1px] border-secondary/60 bg-secondary-smooth-700/10 px-4 py-3 text-sm text-foreground transition-all hover:rounded-full hover:bg-secondary-smooth-700/10">
             <Link href="/pricing">
               <p>Upgrade</p>
             </Link>
