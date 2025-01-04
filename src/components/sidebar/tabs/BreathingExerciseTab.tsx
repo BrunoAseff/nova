2ea6f584e21d1192ff39a2a3a5acea7b3c10e2c2
Bruno Aseff <brunoaseff2@gmail.com>
2025-01-04 15:27:28 -0300

    style: add `BreathingExerciseTab` responsive

--- src/components/sidebar/tabs/BreathingExerciseTab.tsx
+++ src/components/sidebar/tabs/BreathingExerciseTab.tsx
@@ -43,7 +43,7 @@ export default function BreathingExerciseTab() {
   };
 
   return (
-    <main className="h-screen w-[110%]">
+    <main className="h-screen w-[140%] md:w-[110%]">
       <TabHeader
         title="Breathing Exercise"
         subtitle="Configure and select guided breathing techniques.
@@ -52,14 +52,14 @@ export default function BreathingExerciseTab() {
       />
       <div className="scrollbar-thin scrollbar-gutter-stable scrollbar-track-background scrollbar-thumb-accent z-50 mt-36 max-h-[65vh] min-w-[95%] max-w-[115%] space-y-6 overflow-y-auto pr-2">
         <div className="flex min-h-10 w-full items-center justify-between space-x-2 rounded-2xl border-[1px] border-accent/20 bg-accent-foreground p-4">
-          <div className="flex w-full flex-col gap-1">
+          <div className="flex w-[90%] flex-col gap-1">
             <Label
               htmlFor="BreathingExercise-visibility"
               className="text-md text-foreground"
             >
               Visibility
             </Label>
-            <p className="overflow-hidden text-ellipsis whitespace-nowrap text-sm text-muted-foreground">
+            <p className="overflow-hidden text-xs text-muted-foreground md:max-w-none md:text-sm">
               Controls if the Breathing Exercise is visible on the screen.
             </p>
           </div>
