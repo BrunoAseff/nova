9a1700a8c303767b8d114a07f21091720742a919
Bruno Aseff <155178870+BrunoAseff@users.noreply.github.com>
2025-01-22 14:23:31 +0000

    feat: add skeleton to hero section

--- src/components/landing-page/Hero.tsx
+++ src/components/landing-page/Hero.tsx
@@ -1,19 +1,29 @@
 import { Button } from "../ui/button";
+import { Skeleton } from "@/components/ui/skeleton";
+import { useState } from "react";
 
 export default function Hero() {
+  const [isVideoLoaded, setIsVideoLoaded] = useState(false);
+
   return (
-    <section className="z-50 flex w-full items-center justify-evenly">
-      <div className="ml-20 flex flex-col gap-4 text-5xl text-foreground">
+    <section className="z-50 flex w-full flex-col items-center justify-evenly md:flex-row">
+      <div className="flex flex-col items-center justify-center gap-4 px-6 text-2xl text-foreground md:ml-20 md:items-start md:justify-start md:px-0 md:text-5xl">
         <h1>A platform that helps you shine through focus and calm.</h1>
         <Button className="scale-110 font-semibold">Get started</Button>
       </div>
       <div className="relative scale-[0.70] overflow-hidden rounded-lg shadow-[0px_20px_1007px_10px] shadow-secondary/60">
+        {!isVideoLoaded && (
+          <Skeleton className="absolute inset-0 h-full w-full" />
+        )}
         <video
-          className="h-full w-full scale-[1.1] object-cover"
+          className={`h-full w-full scale-[1.1] object-cover ${
+            isVideoLoaded ? "opacity-100" : "opacity-0"
+          }`}
           autoPlay
           loop
           muted
           playsInline
+          onLoadedData={() => setIsVideoLoaded(true)}
         >
           <source src="/pomodoro.mp4" type="video/mp4" />
         </video>
