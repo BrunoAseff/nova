f3a29d34b0ffb49a3709784cd9bc35c6535c2999
Bruno Aseff <155178870+BrunoAseff@users.noreply.github.com>
2025-01-24 12:11:54 +0000

    feat: add pricing to landing page

--- src/components/landing-page/Pricing.tsx
+++ src/components/landing-page/Pricing.tsx
@@ -1,3 +1,91 @@
+import { CheckCircle } from "@phosphor-icons/react";
+import { Label } from "../ui/label";
+import { Button } from "../ui/button";
+import Link from "next/link";
+
 export default function Pricing() {
-  return <section className="min-h-[600px]"></section>;
+  const novaItems = [
+    "Pomodoro",
+    "Background images",
+    "Ambient sounds",
+    "Up to 3 customizable spaces",
+    "Up to 3 reminders",
+    "Only available backgrounds",
+    "Only light and dark themes",
+  ];
+
+  const supernovaItems = [
+    "Everything in free",
+    "10 customizable spaces",
+    "Unlimited remiders",
+    "Upload your own background images",
+    "Access to all themes",
+    "Curated playlists",
+    "Add your custom playlists",
+  ];
+
+  return (
+    <section className="flex w-full flex-col items-center justify-center">
+      <h1 className="my-10 text-4xl font-[600] text-foreground">Pricing</h1>
+
+      <div className="grid w-[65vw] grid-cols-1 items-stretch justify-center gap-4 md:grid-cols-2">
+        <div className="z-50 flex rounded-2xl border-[1px] border-foreground/10 bg-background">
+          <div className="flex w-full flex-col items-center justify-between p-10">
+            <div className="w-full">
+              <Label className="flex w-full justify-between text-2xl font-[600]">
+                <p className="text-foreground">Nova</p>
+                <p>Free</p>
+              </Label>
+              <ul className="mt-6 flex w-full flex-col gap-6 text-muted-foreground">
+                {novaItems.map((item, index) => (
+                  <li className="flex items-center gap-3" key={index}>
+                    <CheckCircle
+                      weight="duotone"
+                      size={18}
+                      className="text-muted-foreground"
+                    />
+                    <p>{item}</p>
+                  </li>
+                ))}
+              </ul>
+            </div>
+            <Button
+              asChild
+              className="mt-10 w-full gap-2 rounded-xl border-[1px] bg-foreground p-3 font-sans text-sm font-[600] text-background shadow-[0px_20px_207px_3px] shadow-transparent transition-all hover:bg-foreground hover:shadow-foreground/10"
+            >
+              <Link href="/spaces">Go to spaces</Link>
+            </Button>
+          </div>
+        </div>
+
+        <div className="z-40 flex rounded-2xl border-[1px] border-secondary/20 bg-background">
+          <div className="flex w-full flex-col items-center justify-between p-10">
+            <div className="w-full">
+              <Label className="flex w-full justify-between bg-none text-2xl font-[600]">
+                <p className="bg-gradient-to-tl from-secondary via-secondary-smooth-500 to-secondary bg-clip-text text-transparent">
+                  Supernova
+                </p>
+                <p>$3 / month</p>
+              </Label>
+              <ul className="mt-6 flex w-full flex-col gap-6 text-muted-foreground">
+                {supernovaItems.map((item, index) => (
+                  <li className="flex items-center gap-3" key={index}>
+                    <CheckCircle
+                      weight="duotone"
+                      size={18}
+                      className="text-muted-foreground"
+                    />
+                    <p>{item}</p>
+                  </li>
+                ))}
+              </ul>
+            </div>
+            <Button className="mt-10 w-full gap-2 rounded-xl border-[1px] bg-gradient-to-r from-secondary via-secondary-smooth-400 to-secondary-smooth-500 p-3 font-sans text-sm font-[600] text-background shadow-[0px_20px_207px_10px] shadow-secondary/60 transition-all hover:bg-secondary-smooth-700/10 hover:shadow-secondary/80">
+              Upgrade now
+            </Button>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
 }
