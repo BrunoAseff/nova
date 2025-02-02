95b31d704d1927a8ed97f7ee6e042cf319103885
Bruno Aseff <155178870+BrunoAseff@users.noreply.github.com>
2025-02-02 17:00:19 +0000

    feat: add button to navbar

--- src/components/landing-page/Nav.tsx
+++ src/components/landing-page/Nav.tsx
@@ -49,52 +49,57 @@ export default function Nav() {
 
 function MobileNav() {
   return (
-    <nav className="fixed z-[98] flex w-full items-center justify-between border-b-[1px] border-secondary/10 bg-secondary/10 px-8 py-1 backdrop-blur-lg md:hidden">
+    <nav className="fixed z-[98] flex w-full items-center justify-between border-b-[1px] border-secondary/10 bg-secondary/10 px-2 py-1 backdrop-blur-lg md:hidden">
       <div className="w-full origin-left scale-50">
         <Logo />
       </div>
-      <Sheet>
-        <SheetTrigger>
-          <List className="text-foreground" size={32} />
-        </SheetTrigger>
-        <SheetContent className="z-[99] flex flex-col gap-10">
-          <SheetTitle className="text-transparent">
-            <div className="w-full origin-left scale-50">
-              <Logo />
+      <div className="flex items-center gap-3">
+        <Button className="block bg-secondary font-semibold md:hidden">
+          <Link href="/spaces">Try for free</Link>
+        </Button>
+        <Sheet>
+          <SheetTrigger>
+            <List className="text-foreground" size={32} />
+          </SheetTrigger>
+          <SheetContent className="z-[99] flex flex-col gap-10">
+            <SheetTitle className="text-transparent">
+              <div className="w-full origin-left scale-50">
+                <Logo />
+              </div>
+            </SheetTitle>
+            <ul className="mt-6 flex flex-col gap-6 text-base text-foreground">
+              <li>
+                <LinkBtn className="cursor-pointer text-foreground">
+                  <SheetClose asChild>
+                    <Link href="#features">Features</Link>
+                  </SheetClose>
+                </LinkBtn>
+              </li>
+              <li>
+                <LinkBtn className="cursor-pointer text-foreground">
+                  <SheetClose asChild>
+                    <Link href="#pricing">Pricing</Link>
+                  </SheetClose>
+                </LinkBtn>
+              </li>
+              <li>
+                <LinkBtn className="cursor-pointer text-foreground">
+                  <SheetClose asChild>
+                    <Link href="#footer">Contact</Link>
+                  </SheetClose>
+                </LinkBtn>
+              </li>
+            </ul>
+            <div className="flex items-center gap-2">
+              <Button className="bg-secondary font-semibold" asChild>
+                <SheetClose asChild>
+                  <Link href="/spaces">Go to Nova</Link>
+                </SheetClose>
+              </Button>
             </div>
-          </SheetTitle>
-          <ul className="mt-6 flex flex-col gap-6 text-base text-foreground">
-            <li>
-              <LinkBtn className="cursor-pointer text-foreground">
-                <SheetClose asChild>
-                  <Link href="#features">Features</Link>
-                </SheetClose>
-              </LinkBtn>
-            </li>
-            <li>
-              <LinkBtn className="cursor-pointer text-foreground">
-                <SheetClose asChild>
-                  <Link href="#pricing">Pricing</Link>
-                </SheetClose>
-              </LinkBtn>
-            </li>
-            <li>
-              <LinkBtn className="cursor-pointer text-foreground">
-                <SheetClose asChild>
-                  <Link href="#footer">Contact</Link>
-                </SheetClose>
-              </LinkBtn>
-            </li>
-          </ul>
-          <div className="flex items-center gap-2">
-            <Button className="bg-secondary font-semibold" asChild>
-              <SheetClose asChild>
-                <Link href="/spaces">Go to Nova</Link>
-              </SheetClose>
-            </Button>
-          </div>
-        </SheetContent>
-      </Sheet>
+          </SheetContent>
+        </Sheet>
+      </div>
     </nav>
   );
 }
